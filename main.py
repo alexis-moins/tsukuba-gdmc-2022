381e2a7aed0985da32df93fc6662263e4f433ecf
AlexisMoins <moinsalexis@gmail.com>
2022-04-19 17:30:22 +0200

    feat: added utils and utility classes Coordinates, Direction and Block

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from __future__ import annotations
-from dataclasses import dataclass, field
-from typing import Dict, List, Tuple
-from random import randint
+from typing import Dict
 
 from gdpc import geometry as GEO
 from gdpc import interface as INTF
 from gdpc import toolbox as TB
 from gdpc import worldLoader as WL
+
+from utils import Block, get_block_at
 
 STARTX, STARTY, STARTZ, ENDX, ENDY, ENDZ = INTF.requestBuildArea()
 
@@ -38,56 +37,6 @@ def get_surface_blocks_count(world: WORLDSLICE) -> Dict[str, int]:
     return surface_blocks
 
 
-def build_house():
-    heights = WORLDSLICE.heightmaps["MOTION_BLOCKING_NO_LEAVES"]
-
-    for x in range(STARTX, ENDX + 1):
-        # the northern wall
-        y = heights[(x, STARTZ)]
-        GEO.placeCuboid(x, y - 2, STARTZ, x, y, STARTZ, most_used_block)
-        GEO.placeCuboid(x, y + 1, STARTZ, x, y + 4, STARTZ, "granite_wall")
-
-        y = heightmap[(x, ENDZ)]
-        GEO.placeCuboid(x, y - 2, ENDZ, x, y, ENDZ, most_used_block)
-        GEO.placeCuboid(x, y + 1, ENDZ, x, y + 4, ENDZ, "red_sandstone_wall")
-
-
-@dataclass(frozen=True)
-class Block:
-    """Represents a block in the world"""
-    name: str
-    coordinates: Tuple[int]
-
-    @property
-    def coordinates_below(self) -> Tuple[int]:
-        """Return the coordinates of the block below"""
-        x, h, z = self.coordinates
-        return x, h - 1, z
-
-    @staticmethod
-    def filter(pattern: str | List[str], blocks: List[Block]) -> List[Block]:
-        """Filter the given list of block and return the ones that contain the given pattern"""
-        if type(pattern) == str:
-            pattern = [pattern]
-
-        iterator = filter(lambda block: block.is_one_of(pattern), blocks)
-        return list(iterator)
-
-    def is_one_of(self, pattern: List[str]) -> bool:
-        """"""
-        for item in pattern:
-            if item in self.name:
-                return True
-        return False
-
-
-def get_block_at(x, h, z) -> Block:
-    """"""
-    coordinates = x, h, z
-    name = WORLDSLICE.getBlockAt(*coordinates)
-    return Block(name, coordinates)
-
-
 if __name__ == '__main__':
 
     height = heightmap[(STARTX, STARTY)]
@@ -106,7 +55,7 @@ if __name__ == '__main__':
     for x, rest in enumerate(WORLDSLICE.heightmaps['MOTION_BLOCKING']):
         for z, h in enumerate(rest):
 
-            block = get_block_at(STARTX + x, h - 1, STARTZ + z)
+            block = get_block_at(STARTX + x, h - 1, STARTZ + z, WORLDSLICE)
             surface_blocks.append(block)
 
     wood_blocks = Block.filter(['leaves', 'log'], surface_blocks)
@@ -114,9 +63,11 @@ if __name__ == '__main__':
     while wood_blocks:
         block = wood_blocks.pop(0)
 
-        block_below = get_block_at(*block.coordinates_below)
-        if block_below.is_one_of(['leaves', 'air', 'log']):
-            wood_blocks.append(block_below)
+        for coordinates in block.neighbouring_coordinates():
+            block_below = get_block_at(*coordinates, WORLDSLICE)
+
+            if block_below.is_one_of(['leaves', 'log']):
+                wood_blocks.append(block_below)
 
         INTF.placeBlock(*block.coordinates, 'air')
-        print(f'Deleted block {block.name} at {block.coordinates}')
+        print(f'Deleted block {block}')
---
+++ utils.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import Iterator, List
+from dataclasses import astuple, dataclass
+
+
+class Direction(Enum):
+    """Enumeration representing all available directions"""
+    UP = (0, 1, 0)
+    DOWN = (0, -1, 0)
+    NORTH = (0, 0, -1)
+    SOUTH = (0, 0, 1)
+    EAST = (1, 0, 0)
+    WEST = (-1, 0, 0)
+
+
+@dataclass(frozen=True)
+class Coordinates:
+    """Represents a set of x, y and z coordinates"""
+    x: int
+    y: int
+    z: int
+
+    def towards(self, direction: Direction) -> Coordinates:
+        """Return the next coordinates in the given direction (from the current coordinates)"""
+        return Coordinates(self.x + direction.value[0], self.y + direction.value[1], self.z + direction.value[2])
+
+    def __iter__(self) -> Iterator:
+        """Return an iterator over the current coordinates"""
+        coordinates = astuple(self)
+        return iter(coordinates)
+
+
+@dataclass(frozen=True)
+class Block:
+    """Represents a block in the world"""
+    name: str
+    coordinates: Coordinates
+
+    def neighbouring_coordinates(self) -> List[Coordinates]:
+        """Return the list of all this block's neighbouring coordinates"""
+        return [self.coordinates.towards(direction) for direction in Direction]
+
+    def is_one_of(self, patterns: List[str]) -> bool:
+        """Return true if the current item's name matches one of the given patterns"""
+        for pattern in patterns:
+            if pattern in self.name:
+                return True
+        return False
+
+    @staticmethod
+    def filter(pattern: str | List[str], blocks: List[Block]) -> List[Block]:
+        """Filter the given list of block and return the ones that contain the given pattern"""
+        if type(pattern) == str:
+            pattern = [pattern]
+
+        iterator = filter(lambda block: block.is_one_of(pattern), blocks)
+        return list(iterator)
+
+
+def get_block_at(x: int, y: int, z: int, world) -> Block:
+    """Return the block found at the given x, y, z coordinates in the world"""
+    name = world.getBlockAt(x, y, z)
+    return Block(name, Coordinates(x, y, z))
