0a0c6d3a299534404da0893fe10f7d0597c6b6c9
Youri <yrus.chan@gmail.com>
2022-04-21 19:01:07 +0200

    Renamed files correctly

--- plots/construction_plot.py
+++ plots/construction_plot.py
@@ -1,132 +1,44 @@
-from __future__ import annotations
+from typing import Tuple
 
-import random
-from typing import Dict, Tuple, Set
-
-import numpy as np
+from plots.plot import Plot
+from utils.coordinates import Coordinates
+from utils.criteria import Criteria
 from gdpc import geometry as GEO
 from gdpc import interface as INTF
 
-from plots.building_plot import ConstructionPlot
-from plots.plot import Plot
-from utils.block import Block
-from utils.coordinates import Coordinates
-from utils.criteria import Criteria
 
+class ConstructionPlot(Plot):
 
-class SuburbPlot(Plot):
-    _WORST_SCORE = 100_000_000
-
-    def __init__(self, x: int, z: int, size: Tuple[int, int], construction_roof: int = 200):
+    def __init__(self, x: int, z: int, size: Tuple[int, int], build_start: Coordinates):
         super().__init__(x, z, size)
+        self.build_start = build_start
 
-        self.construction_roof = construction_roof
-        # Surface word here is to precise that we are working
-        self.occupied_coords_surface: Set[Coordinates] = set()
-        self.foundation_blocks_surface: Dict[Coordinates, Block] = dict()
+    def build_foundation(self, foundation_level: int, main_block: str = 'stone_bricks') -> None:
 
-    def _build_foundation_blocks(self) -> None:
-        self.foundation_blocks_surface = {b.coordinates.as_2D(): b for b in filter(self._is_block_valid,
-                                                                                   self.get_blocks_at_surface(
-                                                                                       Criteria.MOTION_BLOCKING_NO_LEAVES))}
-
-    def _is_block_valid(self, b):
-        return b.coordinates.y < self.construction_roof and not b.is_one_of(
-            ["water"]) and b.coordinates.as_2D() not in self.occupied_coords_surface
-
-    def get_construction_plot(self, size: Tuple[int, int], padding: int = 3, speed: int = None) -> ConstructionPlot | None:
-        """Return the best coordinates to place a building of a certain size, minimizing its score.
-            Score is defined by get_score function.
-
-            heightmap
-            """
-        if speed is None:
-            # Auto speed depends on structure size, and is at least 1
-            # Todo : Should depend on plot size too
-            # speed = max(max(size[0], size[1]) // 5, 1)
-            # print(f"Auto determined speed {speed} for house of size {size}")
-            speed = 1
-
-        # This will update the foundation_block dict, as well as the _construction_heightmap
-        self._build_foundation_blocks()
-
-        # DEBUG
-        colors = ['green', 'pink', 'magenta', 'lime', 'yellow', 'orange', 'purple', 'gray', 'white']
-        random.shuffle(colors)
-        for coord_2d in self.foundation_blocks_surface:
-            INTF.placeBlock(*self.foundation_blocks_surface[coord_2d].coordinates, colors[0] + '_wool')
-
-        INTF.sendBlocks()
-        # END DEBUG
-
-        keys_list = list(self.foundation_blocks_surface.keys())
-
-        # >Get the minimal score in the coordinate list
-        min_score = SuburbPlot._WORST_SCORE
-        best_coord_2d = keys_list[0]
-
-        for coord_2d in keys_list[::speed]:
-            coord_score = self._get_score(coord_2d, size)
-
-            if coord_score < min_score:
-                best_coord_2d = coord_2d
-                min_score = coord_score
-
-        print(f'Best score : {min_score}')
-
-        if min_score == SuburbPlot._WORST_SCORE:
-            return None
-
-        best_coord = self.foundation_blocks_surface[best_coord_2d].coordinates
-
-        self.occupy_area(best_coord, size, padding)
-
-        return ConstructionPlot(x=best_coord_2d.x, z=best_coord_2d.z, size=size, build_start=best_coord)
-
-    def _get_score(self, coord_2d: Coordinates, size: Tuple[int, int]) -> float:
-        """Return a score evaluating the fitness of a building in an area.
-            The lower the score, the better it fits
-
-            Score is calculated as follows :
-            malus depending on the distance from the center of the area +
-            Sum of all differences in the y coordinate
-            """
-        # apply malus to score depending on the distance to the 'center'
-        # Todo : Maybe improve this notation, quite not beautiful, set center as a coordinate ? Would be great
-        score = coord_2d.distance(Coordinates(self.center[0], 0, self.center[1])) * .1
-
-        evaluated_coord_y = self.foundation_blocks_surface[coord_2d].coordinates.y
-
-        # Score = sum of difference between the first point's altitude and the other
-        for x in range(size[0]):
-            for z in range(size[1]):
-                try:
-                    current = coord_2d.shift(x, 0, z)
-                    current_y = self.foundation_blocks_surface[current].coordinates.y
-
-                    score += abs(evaluated_coord_y - current_y)
-
-                except KeyError:
-                    # Out of bound :3
-                    return SuburbPlot._WORST_SCORE
-        return score
-
-    def occupy_area(self, origin: Coordinates, size: Tuple[int, int], padding: int = 0) -> None:
-        """Set all the coordinates in the size given, starting from the origin, and in the padding, as occupied, and so
-        unusable as foundations for other constructions"""
-        for x in range(-padding, size[0] + padding):
-            for z in range(-padding, size[1] + padding):
-
-                coord_2d = origin.shift(x, 0, z).as_2D()
-                if coord_2d in self:
-                    self.occupied_coords_surface.add(coord_2d)
-
-        # ONLY FOR DEBUG :D
-        for coord_2d in self.occupied_coords_surface:
-            try:
-                INTF.placeBlock(*self.foundation_blocks_surface[coord_2d].coordinates, 'red_wool')
-            except KeyError:
-                pass
+        for coord in self._iterate_over_air(foundation_level):
+            INTF.placeBlock(*coord, main_block)
         INTF.sendBlocks()
 
+    def _iterate_over_air(self, max_y: int) -> Coordinates:
+        for block in self.get_blocks_at_surface(Criteria.WORLD_SURFACE):
+            y_shift = 1
+            while block.coordinates.y + y_shift <= max_y:
+                yield block.coordinates.shift(0, y_shift, 0)
+                y_shift += 1
 
+    def build_simple_house(self, main_bloc: str, height: int):
+        """Build a 'house' of the main_bloc given, with north-west bottom corner as starting point, with the given size"""
+        # Todo : finish the simple house
+
+        self.build_foundation(self.build_start.y - 1)
+
+        # body
+        GEO.placeCuboid(self.build_start.x, self.build_start.y, self.build_start.z, self.build_start.x + self.size[0] - 1,
+                        self.build_start.y + height - 1, self.build_start.z + self.size[1] - 1,
+                        main_bloc, hollow=True)
+
+        # Todo : add direction
+        # Door
+        INTF.placeBlock(self.build_start.x + self.size[0] // 2, self.build_start.y + 1, self.build_start.z, "oak_door")
+        INTF.placeBlock(self.build_start.x + self.size[0] // 2, self.build_start.y + 2, self.build_start.z, "oak_door[half=upper]")
+        INTF.sendBlocks()
