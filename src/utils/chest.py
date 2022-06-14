9a5a4f051e47cea6e5ec12b1af0a02c72259bc33
Youri <itrotky@gmail.com>
2022-06-14 09:14:10 +0200

    made probability to getting treasure coord 1

--- src/utils/chest.py
+++ src/utils/chest.py
@@ -7,7 +7,7 @@ def get_filled_chest_data(prio_items, table, fill_amount: int = None):
     slots = random.sample(range(27), k=fill_amount + 1)  # + 1 if we need slot for prio item
     item_data = [item.to_minecraft_data(slot) for slot, item in zip(slots, list(table.get_items(fill_amount)))]
     # Add prio item
-    if prio_items and random.randint(1, 2) == 1:  # One chance / 2
+    if prio_items:
         item_data.append(random.choice(prio_items).to_minecraft_data(slots[-1]))
     chest_data = f'{{Items:[{",".join(item_data)}]}}'
     return chest_data
